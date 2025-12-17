18a6f68ae02ec94efe8c60d80bdebadc2fff8d1c
Muhammad Junaid Farooq <junaidfarooq.pk@gmail.com>
2025-12-17 19:45:35 +0500

    feat(post): implement create post API with media support

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -1,6 +1,30 @@
-
+import User from "../models/user.model.js";
+import Post from "../models/posts.model.js";
 
 export const activeCheck = async (req, res) => {
   return res.status(200).json({ message: "RUNNING" });
 };
 
+export const createPost = async (req, res) => {
+  const { token } = req.body;
+  try {
+    const user = await User.findOne({ token: token });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const post = new Post({
+      userId: user._id,
+      body: req.body.body,
+      media: req.file != undefined ? req.file.filename : "",
+      fileType: req.file != undefined ? req.file.mimetype.split("/")[1] : "",
+    });
+
+    await post.save();
+
+    return res.status(201).json({ message: "Post created successfully", post });
+  } catch (error) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
