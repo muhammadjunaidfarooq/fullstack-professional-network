a0bf28290ee9df7ce0d25628e130759af9adf220
Muhammad Junaid Farooq <junaidfarooq.pk@gmail.com>
2025-12-18 08:29:52 +0500

    feat(auth): implement login and register using Redux async thunks

---
+++ frontend/src/config/redux/action/authAction/index.js
@@ -0,0 +1,43 @@
+import { clientServer } from "@/config";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const loginUser = createAsyncThunk(
+  "user/login",
+  async (user, thunkAPI) => {
+    try {
+      const response = await clientServer.post("/login", {
+        email: user.email,
+        password: user.password,
+      });
+
+      if (response.data.token) {
+        localStorage.setItem("token", response.data.token);
+      } else {
+        return thunkAPI.rejectWithValue({ message: "No token received" });
+      }
+
+      return thunkAPI.fulfillWithValue(response.data.token);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const registerUser = createAsyncThunk(
+  "user/register",
+  async (user, thunkAPI) => {
+    try {
+      const response = await clientServer.post("/register", {
+        username: user.username,
+        password: user.password,
+        email: user.email,
+        name: user.name,
+      });
+
+      return thunkAPI.fulfillWithValue(response.data);
+
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
